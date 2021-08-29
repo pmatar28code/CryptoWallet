16f69be343c3711d30be074c5be360ed61b07544
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-08-29 18:11:54 -0500

    send money2FaDialog now fills the field automatically with twilio message request in function, added twilio class , api, basicAuth interceptor, and code to clean message in same fun, tested on emulator and phone
    send money2FaDialog now fills the field automatically with twilio message request in function, added twilio class , api, basicAuth interceptor, and code to clean message in same fun, tested on emulator and phone

--- app/src/main/java/com/example/cryptowallet/dialog/SendMoney2FaDialog.kt
+++ app/src/main/java/com/example/cryptowallet/dialog/SendMoney2FaDialog.kt
@@ -2,12 +2,25 @@ package com.example.cryptowallet.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.fragment.app.DialogFragment
 import com.example.cryptowallet.R
 import com.example.cryptowallet.Repository
 import com.example.cryptowallet.databinding.FragmentSendMoney2faDialogBinding
+import com.example.cryptowallet.twilio.BasicAuthInterceptor
+import com.example.cryptowallet.twilio.TwilioApi
+import com.example.cryptowallet.twilio.TwilioReadMessages
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class SendMoney2FaDialog: DialogFragment() {
     companion object {
@@ -23,12 +36,25 @@ class SendMoney2FaDialog: DialogFragment() {
         val inflater = LayoutInflater.from(requireContext())
         val binding = FragmentSendMoney2faDialogBinding.inflate(inflater)
 
+        val accountSID = "AC41847eef5c541ba476941edcbfd2e51a"
+        val authToken = "1a82d46faa314f5aef0c1353ff3e9b60"
+
+        runBlocking {
+            delay(4000)
+            automaticallyGet2FATokenFromTwilio(accountSID, authToken) {
+                binding.outlinedTextField2FaToken.editText?.setText(it)
+                Repository.token2fa = binding.outlinedTextField2FaToken.editText?.text.toString()
+            }
+        }
+
         return MaterialAlertDialogBuilder(
             requireContext(), R.style.MyRounded_MaterialComponents_MaterialAlertDialog
         )
             .setView(binding.root)
             .setPositiveButton(getString(R.string.dialog_send)) { _, _ ->
-                setPositiveButton(binding)
+                //setPositiveButton(binding)
+                Log.e("TOKEN 2FA IN REPO TEST TWILIO:", Repository.token2fa)
+                onItemAddedListener()
             }
             .setNegativeButton(getString(R.string.dialog_cancel), null)
             .create()
@@ -36,5 +62,43 @@ class SendMoney2FaDialog: DialogFragment() {
     private fun setPositiveButton(binding: FragmentSendMoney2faDialogBinding){
         Repository.token2fa = binding.outlinedTextField2FaToken.editText?.text.toString()
         onItemAddedListener()
+    }
+
+    private fun automaticallyGet2FATokenFromTwilio(accountSID:String,authToken:String,twoFaTokenTwilioCallback:(String) -> Unit){
+        val logger = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(BasicAuthInterceptor(accountSID,authToken))
+            .addInterceptor(logger)
+            .build()
+
+        val retrofitBuilder = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://api.twilio.com/2010-04-01/Accounts/")
+            .addConverterFactory(MoshiConverterFactory.create())
+        val retrofit = retrofitBuilder.build()
+        val twilioClient = retrofit.create(TwilioApi::class.java)
+        val twilioCall = twilioClient.getMessages()
+        twilioCall.enqueue(object: Callback<TwilioReadMessages> {
+            override fun onResponse(call: Call<TwilioReadMessages>, response: Response<TwilioReadMessages>) {
+                val theActualMessageWithToken = response.body()?.messages?.get(1)?.body.toString()
+                val actualCleanTokenFromMessage = getTheCorrectTokenMessageAndCleanIt(theActualMessageWithToken)
+                twoFaTokenTwilioCallback(actualCleanTokenFromMessage)
+            }
+            override fun onFailure(call: Call<TwilioReadMessages>, t: Throwable) {
+                Log.e("ON FAILURE","$t")
+                twoFaTokenTwilioCallback("Did not get the Token")
+            }
+        })
+    }
+    fun getTheCorrectTokenMessageAndCleanIt(theActualMessageWithToken:String):String{
+        var theCorrectMessageWithToken =""
+        for(char in theActualMessageWithToken){
+            if(char != ' ' && char != '.' && char != ';' && char != 'Y'){
+                theCorrectMessageWithToken+=char
+            }
+        }
+        Log.e("RETURN CORRECT MESSAGE WITH CLEAN TOKEN:", theCorrectMessageWithToken)
+        return theCorrectMessageWithToken
     }
 }
